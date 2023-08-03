512a556611589120e5b4e0b1f9899e3aaff33c7c
Khalid Hassan <khalidhassan218@outlook.com>
2023-08-03 04:39:40 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
+import axios from 'axios';
 function App() {
+  const [startValue, setStartValue] = useState(350000);
+  const [startDate, setStartDate] = useState('2018KW02');
+  const [currentDate, setCurrentDate] = useState('2023KW01');
+  const [region, setRegion] = useState('GM0363');
+  const [currentHomeValue, setCurrentHomeValue] = useState(null);
+
+  const handleCalculate = () => {
+    const data = {
+      start_value: startValue,
+      start_date: startDate,
+      current_date: currentDate,
+      region: region,
+    };
+
+    axios.post('https://home-equity.adaptable.app/calculate_home_value', data)
+      .then(response => {
+        setCurrentHomeValue(response.data.current_home_value);
+      })
+      .catch(error => {
+        console.error('Error calculating home value:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="number" value={startValue} onChange={(e) => setStartValue(parseFloat(e.target.value))} />
+      <input type="text" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+      <input type="text" value={currentDate} onChange={(e) => setCurrentDate(e.target.value)} />
+      <input type="text" value={region} onChange={(e) => setRegion(e.target.value)} />
+      <button onClick={handleCalculate}>Calculate Home Value</button>
+      {currentHomeValue !== null && <div>Current home value: {currentHomeValue} Euros</div>}
     </div>
   );
 }
